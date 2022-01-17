fdda0201c3fe729ad01482abe6e562fb30f1d2f0
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2022-01-17 13:36:04 +0100

    Ajoute le contenu de la séance 03.
    Les objectifs étaient:
    - Installation de mypy, black, pytest
    - Tests automatiques de la librairie graphe
    - Documentation systématique de la librairie graphe
    - Vérification du typage avec mypy
    - Formattage automatique du code avec black
    - Notion publique d'interface d'un module

---
+++ Seance03/graphe.py
@@ -0,0 +1,95 @@
+"""Description.
+
+Librairie contenant une fonction permettant de calculer des chemins dans un graphe.
+"""
+
+from typing import TypeVar
+
+S = TypeVar("S")
+A = tuple[S, S]
+
+
+class PasDeChemin(Exception):
+    pass
+
+
+def _calcule_voisins(depart: S, arretes: list[A]) -> list[S]:
+    """Renvoie la liste des sommets accessibles en un pas depuis depart.
+
+    Exemples:
+    >>> _calcule_voisins(depart=1, arretes=[(1, 2), (1, 3)])
+    [2, 3]
+    >>> _calcule_voisins(depart=1, arretes=[(1, 1), (1, 2), (1, 3)])
+    [2, 3]"""
+    return [e2 for (e1, e2) in arretes if e1 == depart and e2 != depart]
+
+
+def _elague_sommets(a_enlever: S, sommets: list[S]) -> list[S]:
+    """Enleve le sommet de la liste.
+
+    Exemples:
+    >>> _elague_sommets(a_enlever=1, sommets=[1, 2, 3])
+    [2, 3]
+    >>> _elague_sommets(a_enlever=4, sommets=[1, 2, 3])
+    [1, 2, 3]"""
+    return [etat for etat in sommets if a_enlever != etat]
+
+
+def _elague_arretes(a_enlever: S, arretes: list[A]) -> list[A]:
+    """Enleve les références à a_enlever de la liste des arrêtes.
+
+    Exemples
+    >>> _elague_arretes(a_enlever=1, arretes=[(1, 1), (1, 2), (1, 3)])
+    []
+    >>> _elague_arretes(a_enlever=1, arretes=[(1, 1), (1, 2), (1, 3), (2, 3)])
+    [(2, 3)]"""
+
+    return [(e1, e2) for (e1, e2) in arretes if e1 != a_enlever and e2 != a_enlever]
+
+
+def calcule_chemin(
+    depart: S, arrivee: S, sommets: list[S], arretes: list[A]
+) -> list[S]:
+    """Détermine un chemin de depart à arrivee dans le graphe.
+
+    Si un tel chemin n'existe pas l'exception PasDeChemin est soulevée.
+    On trouve toujours un chemin sans boucle.
+
+    Exemples:
+    >>> calcule_chemin(
+    ...         depart=1,
+    ...         arrivee=4,
+    ...         sommets=[1, 2, 3, 4],
+    ...         arretes=[(1, 1), (1, 2), (1, 3), (2, 4)]
+    ...     )
+    [1, 2, 4]
+    >>> calcule_chemin(
+    ...             depart=4,
+    ...             arrivee=1,
+    ...             sommets=[1, 2, 3, 4],
+    ...             arretes=[(1, 1), (1, 2), (1, 3), (2, 4)]
+    ...         )
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+      File "C:\\Users\\perrollaz\\Documents\\graphe.py", line 51, in calcule_chemin
+
+    __main__.PasDeChemin
+    """
+    if depart == arrivee:
+        return [depart]
+    sommets_elagues = _elague_sommets(a_enlever=depart, sommets=sommets)
+    arretes_elaguees = _elague_arretes(a_enlever=depart, arretes=arretes)
+    for voisin in _calcule_voisins(depart, arretes):
+        try:
+            resultat_intermediaire = calcule_chemin(
+                depart=voisin,
+                arrivee=arrivee,
+                sommets=sommets_elagues,
+                arretes=arretes_elaguees,
+            )
+        except PasDeChemin:
+            pass
+        else:
+            return [depart] + resultat_intermediaire
+
+    raise PasDeChemin
---
+++ Seance03/lib.py
@@ -0,0 +1,100 @@
+"""Description.
+
+Librairie permettant de construire le graphe associé au problème de rivière.
+"""
+
+
+from enum import Enum
+from dataclasses import dataclass
+
+
+class Rive(Enum):
+    GAUCHE = "gauche"
+    DROITE = "droite"
+
+
+@dataclass
+class Etat:
+    berger: Rive
+    loup: Rive
+    mouton: Rive
+    chou: Rive
+
+
+def est_valide(etat: Etat) -> bool:
+    """Vérifie si la règle de supervision est respectée."""
+    if etat.berger != etat.loup and etat.loup == etat.mouton:
+        return False
+    if etat.berger != etat.chou and etat.chou == etat.mouton:
+        return False
+    return True
+
+
+def sont_connectes(depart: Etat, arrivee: Etat) -> bool:
+    if depart.berger == arrivee.berger:
+        return False
+
+    nombre_changements = 0
+    if depart.loup != arrivee.loup:
+        nombre_changements += 1
+    if depart.mouton != arrivee.mouton:
+        nombre_changements += 1
+    if depart.chou != arrivee.chou:
+        nombre_changements += 1
+
+    if nombre_changements <= 1:
+        return True
+    else:
+        return False
+
+
+def genere_message(depart: Etat, arrivee: Etat) -> str:
+    if depart.loup != arrivee.loup:
+        return f"Berger, Loup  : {depart.loup} -> {arrivee.loup}"
+    if depart.mouton != arrivee.mouton:
+        return f"Berger, Mouton: {depart.mouton} -> {arrivee.mouton}"
+    if depart.chou != arrivee.chou:
+        return f"Berger, Chou  : {depart.chou} -> {arrivee.chou}"
+    return f"Berger        : {depart.berger} -> {arrivee.berger}"
+
+
+def visualise_chemin(etats: list[Etat]) -> str:
+    resultat = list()s
+    for e1, e2 in zip(etats[:-1], etats[1:]):
+        if not sont_connectes(e1, e2):
+            return "Ce n'est pas un chemin."
+        resultat.append(genere_message(depart=e1, arrivee=e2))
+    return "\n".join(resultat)
+
+
+Arrete = tuple[Etat, Etat]
+
+DEPART = Etat(
+    berger=Rive.DROITE, mouton=Rive.DROITE, loup=Rive.DROITE, chou=Rive.DROITE
+)
+
+ARRIVEE = Etat(
+    berger=Rive.GAUCHE, mouton=Rive.GAUCHE, loup=Rive.GAUCHE, chou=Rive.GAUCHE
+)
+
+ETATS: list[Etat] = [
+    Etat(
+        berger=choix_berger,
+        loup=choix_loup,
+        mouton=choix_mouton,
+        chou=choix_chou,
+    )
+    for choix_berger in Rive
+    for choix_loup in Rive
+    for choix_mouton in Rive
+    for choix_chou in Rive
+]
+
+SOMMETS: list[Etat] = [etat for etat in ETATS if est_valide(etat)]
+
+ARRETES: list[Arrete] = [
+    (e1, e2)
+    for e1 in SOMMETS
+    for e2 in SOMMETS
+    if sont_connectes(depart=e1, arrivee=e2)
+]
