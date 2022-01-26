4596642959890b6bfd50b1686b8cdb27cb621163
Vincent Perrollaz <vincent.perrollaz@laposte.net>
2022-01-26 21:32:52 +0100

    Reprend le contenu de la séance 5.
    - Ajuste le code pour passer tous les tests.
    - Fini la documentation.

--- Seance05/lib.py
+++ Seance05/lib.py
@@ -45,6 +45,44 @@ class Grille:
     On stocke les cases dans une liste 1d lignes par lignes.
 
     Une case vide contient None.
+
+    Exemple:
+    >>> grille = Grille([1, 2, 3, 4, None, 4, 1, 2, 2, None, None, None, 4, 3, 2, 1])
+    >>> repr(grille)
+    'Grille(cases=[1, 2, 3, 4, None, 4, 1, 2, 2, None, None, None, 4, 3, 2, 1])'
+    >>> print(grille)
+
+    -----------------
+    | 1 | 2 | 3 | 4 |
+    -----------------
+    | _ | 4 | 1 | 2 |
+    -----------------
+    | 2 | _ | _ | _ |
+    -----------------
+    | 4 | 3 | 2 | 1 |
+    -----------------
+    >> for case in grille:
+    ...     print(case, end=" ")
+    ...
+    1 2 3 4 None 4 1 2 2 None None None 4 3 2 1
+    >>> grille1 = Grille(cases=[1, 2, 3, 4, 4, 3, 2, 1, 2, 1, 4, 3, 3, 4, 1, 2])
+    >>> grille2 = Grille(cases=[1, 2, 3, 4, 4, 3, 2, 1, 2, 1, 4, 3, 3, 4, 1, 2])
+    >>> grille1 == grille2
+    True
+    >>> Grille(cases=[])
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "./Seance05/lib.py", line 52, in __init__
+        raise ValueError("Il doit y avoir 16 cases exactement.")
+    ValueError: Il doit y avoir 16 cases exactement.
+    >>> valeurs = [None for _ in range(16)]
+    >>> valeurs[5] = 5
+    >>> Grille(cases=valeurs)
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "./lib.py", line 54, in __init__
+        raise ValueError("Les cases ne peuvent contenir que None 1 2 3 ou 4!")
+    ValueError: Les cases ne peuvent contenir que None 1 2 3 ou 4!
     """
 
     def __init__(self, cases: list[Optional[int]]):
@@ -89,6 +127,15 @@ class Grille:
 
 
 def _doublon_present(valeurs: list[Optional[int]]) -> bool:
+    """Teste si un chiffre entre 1 et 4 est répété au moins une fois.
+
+    Exemples:
+    >>> _doublon_present(valeurs=[1, 2, 3])
+    False
+    >>> _doublon_present(valeurs=[1, 1, 2])
+    True
+    >>> _doublon_present(valeurs=[None, None])
+    False"""
     for temoin in range(1, 5):
         if sum(1 for valeur in valeurs if valeur == temoin) > 1:
             return True
@@ -96,6 +143,14 @@ def _doublon_present(valeurs: list[Optional[int]]) -> bool:
 
 
 def _verifie_probleme_lignes(grille: Grille) -> bool:
+    """Vérifie si une des lignes contient une répétition.
+
+    Exemple:
+    >>> valeurs = [None for _ in range(16)]
+    >>> valeurs[0], valeurs[1] = 1, 1
+    >>> _verifie_probleme_lignes(grille=Grille(cases=valeurs))
+    True
+    """
     for i in range(0, 4):
         if _doublon_present([grille[4 * i + j] for j in range(0, 4)]):
             return True
@@ -103,6 +158,13 @@ def _verifie_probleme_lignes(grille: Grille) -> bool:
 
 
 def _verifie_probleme_colonnes(grille: Grille) -> bool:
+    """Vérifie si une des colonnes contient une répétition.
+
+    Exemple:
+    >>> valeurs = [None for _ in range(16)]
+    >>> valeurs[0], valeurs[4] = 1, 1
+    >>> _verifie_probleme_colonnes(grille=Grille(cases=valeurs))
+    True"""
     for j in range(0, 4):
         if _doublon_present([grille[4 * i + j] for i in range(0, 4)]):
             return True
@@ -110,6 +172,14 @@ def _verifie_probleme_colonnes(grille: Grille) -> bool:
 
 
 def _verifie_probleme_carres(grille: Grille) -> bool:
+    """Vérifie si un des sous carrés contient une répétition.
+
+    Exemple:
+    >>> valeurs = [None for _ in range(16)]
+    >>> valeurs[0], valeurs[5] = 1, 1
+    >>> _verifie_probleme_carres(grille=Grille(cases=valeurs))
+    True
+    """
     for indice_carre in (0, 2, 8, 10):
         if _doublon_present(
             [grille[indice_carre + indice_interne] for indice_interne in (0, 1, 4, 5)]
@@ -119,6 +189,15 @@ def _verifie_probleme_carres(grille: Grille) -> bool:
 
 
 def est_valide(grille: Grille) -> bool:
+    """Vérifie si la grille est valide.
+
+    Exemple:
+    >>> grille1 = Grille(cases=[None for _ in range(16)])
+    >>> est_valide(grille1)
+    True
+    >>> grille2 = Grille(cases=[1, 2, 3, 4, None, 4, 1, 2, 2, None, None, None, 4, 3, 2, 1])
+    >>> est_valide(grille2)
+    True"""
     if _verifie_probleme_lignes(grille):
         return False
     if _verifie_probleme_colonnes(grille):
@@ -129,6 +208,16 @@ def est_valide(grille: Grille) -> bool:
 
 
 def est_complete(grille: Grille) -> bool:
+    """Vérifie si toutes les cases sont remplies.
+
+    Exemple:
+    >>> grille_vide = Grille(cases=[None for _ in range(16)])
+    >>> est_complete(grille_vide)
+    False
+    >>> grille_pleine = Grille(cases=[1, 2, 3, 4, 3, 4, 1, 2, 2, 1, 4, 3, 4, 3, 2, 1])
+    >>> est_complete(grille_pleine)
+    True
+    """
     for case in grille:
         if case is None:
             return False
@@ -136,7 +225,24 @@ def est_complete(grille: Grille) -> bool:
 
 
 def _cherche_premier_none(valeurs: list[Optional[int]]) -> int:
-    """Génère une exception si il n'y a pas de None."""
+    """Renvoie l'indice du premier élément valant None.
+
+    Génère une exception si il n'y a pas de None.
+
+    Exemple:
+    >>> _cherche_premier_none([None, 1, 2])
+    0
+    >>> _cherche_premier_none([1, None, 1, 2])
+    1
+    >>> _cherche_premier_none([None, 1, None, 2])
+    0
+    >>> _cherche_premier_none([1, 2, 3])
+    Traceback (most recent call last):
+    File "<stdin>", line 1, in <module>
+    File "./lib.py", line 143, in _cherche_premier_none
+
+    ValueError: Il n'y a pas de None dans cette liste.
+    """
     for indice, valeur in enumerate(valeurs):
         if valeur is None:
             return indice
@@ -144,6 +250,69 @@ def _cherche_premier_none(valeurs: list[Optional[int]]) -> int:
 
 
 def _remplit_une_case(grille: Grille) -> Generator[Grille, None, None]:
+    """Renvoie un itérateurs des nouvelles grilles en remplissant la première case vide avec 1, 2, 3 puis 4.
+
+    Exemple:
+    >>> test = Grille(cases=[None for _ in range(16)])
+    >>> print(test)
+
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+
+    >>> for nouvelle in _remplit_une_case(grille=test):
+    ...     print(nouvelle)
+    ...
+
+    -----------------
+    | 1 | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+
+
+    -----------------
+    | 2 | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+
+
+    -----------------
+    | 3 | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+
+
+    -----------------
+    | 4 | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    | _ | _ | _ | _ |
+    -----------------
+    """
     cases = [case for case in grille]
     try:
         indice = _cherche_premier_none(cases)
@@ -170,6 +339,13 @@ def _remplit_une_case_liste(grille: Grille) -> list[Grille]:
 
 
 def calcule_solutions(grille: Grille) -> list[Grille]:
+    """Fonction renvoyant la liste des toutes les solutions correspondant à une grille partiellement remplie.
+
+    Exemple:
+    >>> calcule_solutions(grille=Grille(cases=[1, 2, 3, 4, 4, None, 2, 1, 2, 1, None, 3, 3, None, 1, 2]))
+    [Grille(cases=[1, 2, 3, 4, 4, 3, 2, 1, 2, 1, 4, 3, 3, 4, 1, 2])]
+    >>> calcule_solutions(grille=Grille(cases=[1, 2, 3, 4, 4, 1, 2, 1, 2, 1, None, 3, 3, None, 1, 2]))
+    []"""
     if not est_valide(grille):
         return []
     if est_complete(grille):
